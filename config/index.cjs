8f19fb5ed9230dcd3e811e4b7a521d70c57f280d
Jin <110438059+Saury0127@users.noreply.github.com>
2022-10-19 21:07:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -40,7 +40,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 认识的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-09-26' },
         // 结婚纪念日
       //  { keyword: 'marry_day', date: '2022-09-09' },
       ],
