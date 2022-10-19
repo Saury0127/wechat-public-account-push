2fc1247a53d5e1b3929a5f8efc90e90c41630d93
Jin <110438059+Saury0127@users.noreply.github.com>
2022-10-19 20:35:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ov3fm6FGVvrZ35ZVScJKsb4CbVHs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Kc3YYc_gsZkg166jBB0BZZud6ciVvg_NrpWrF05dy2s',
+      useTemplateId: 'hQ_HApReuh7ifjAYu049GYgt6PIUpQzNDKOvuEphEkI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-14',
       festivals: [
