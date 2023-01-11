2f69c880885e9bf85480cb63e5491617d5b484e2
Jin <110438059+Saury0127@users.noreply.github.com>
2023-01-11 10:12:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -12,14 +12,14 @@ const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'f429e1a8ef701644adf29985fbd5ddc8',
-  
+
   PROVINCE: '甘肃',
-  CITY: '临夏',
+  CITY: '兰州',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝宝',
+      name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -29,13 +29,12 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '何同学', year: '1999', date: '12-08',
+          type: '*生日', name: '宝', year: '1999', date: '12-08',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-   
-       // {
-         // type: '节日', name: '相识纪念日', year: '2022', date: '09-26',
-       // },
+        {
+          type: '生日', name: '相识纪念日', year: '2022', date: '09-26',
+        }，
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
@@ -49,7 +48,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'nrgm9eZJ3ly8b9djQS9Ab6j--mll1YJ0iK_TyKbi7Os',
+  CALLBACK_TEMPLATE_ID: '2tAIW_aiUFpcYr8M55zonCsadyTA1r8b6CZkngYZY10',
 
   CALLBACK_USERS: [
     {
