e85b4bfde4b1c8005d69e00dd4c7796ae3adc031
Jin <110438059+Saury0127@users.noreply.github.com>
2023-01-11 10:39:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ov3fm6FGVvrZ35ZVScJKsb4CbVHs',
+      id: 'ov3fm6PxxyyklbvapAXlHsdJw8xI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '2tAIW_aiUFpcYr8M55zonCsadyTA1r8b6CZkngYZY10',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -29,7 +29,7 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝', year: '1999', date: '12-08',
+          type: '*生日', name: '宝', year: '2000', date: '01-14',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         //{
@@ -48,7 +48,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '2tAIW_aiUFpcYr8M55zonCsadyTA1r8b6CZkngYZY10',
+  CALLBACK_TEMPLATE_ID: 'rcLbu0meIqPQ4Jk9JaQXTMCktBa1K5W5JeAARPBexw8',
 
   CALLBACK_USERS: [
     {
